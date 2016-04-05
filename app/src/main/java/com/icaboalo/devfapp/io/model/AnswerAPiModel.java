a9fd5efefef7d341b5662605d1953c32f4d1a405
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-05 16:10:21 -0500

    answer api model

--- app/src/main/java/com/icaboalo/devfapp/io/model/AnswerAPiModel.java
+++ app/src/main/java/com/icaboalo/devfapp/io/model/AnswerAPiModel.java
@@ -1,7 +1,52 @@
 package com.icaboalo.devfapp.io.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 /**
  * Created by icaboalo on 5/04/16.
  */
 public class AnswerAPiModel {
+
+    public AnswerAPiModel(String answerLink, Date answerDate, int challengeId, int myUserId) {
+        mAnswerLink = answerLink;
+        mAnswerDate = answerDate;
+        mChallengeId = challengeId;
+        mMyUserId = myUserId;
+    }
+
+    @SerializedName("id")
+    int mAnswerId;
+
+    @SerializedName("file_link")
+    String mAnswerLink;
+
+    @SerializedName("date_added")
+    Date mAnswerDate;
+
+    @SerializedName("challenge")
+    ChallengeApiModel mChallenge;
+
+    @SerializedName("challenge_id")
+    int mChallengeId;
+
+    @SerializedName("my_user_id")
+    int mMyUserId;
+
+    public int getAnswerId() {
+        return mAnswerId;
+    }
+
+    public String getAnswerLink() {
+        return mAnswerLink;
+    }
+
+    public Date getAnswerDate() {
+        return mAnswerDate;
+    }
+
+    public ChallengeApiModel getChallenge() {
+        return mChallenge;
+    }
 }
