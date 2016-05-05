9f1e7b1d7ddbc1c2c5e116477ee927c01d56b374
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-05 07:57:42 -0500

    new start

--- app/src/main/java/com/icaboalo/devfapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/icaboalo/devfapp/ui/activity/MainActivity.java
@@ -1,26 +1,18 @@
 package com.icaboalo.devfapp.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.icaboalo.devfapp.R;
-import com.icaboalo.devfapp.ui.fragment.ChallengeListFragment;
-import com.icaboalo.devfapp.ui.fragment.EventListFragment;
-import com.icaboalo.devfapp.ui.fragment.FileListFragment;
-import com.icaboalo.devfapp.util.VUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
-        replaceFragment(new FileListFragment());
+//        replaceFragment(new FileListFragment());
         navigationViewClick();
     }
 
@@ -50,13 +42,13 @@ public class MainActivity extends AppCompatActivity {
                 Fragment nFragment = null;
                 switch (item.getItemId()) {
                     case R.id.files:
-                        nFragment = new FileListFragment();
+//                        nFragment = new FileListFragment();
                         break;
                     case R.id.events:
-                        nFragment = new EventListFragment();
+//                        nFragment = new EventListFragment();
                         break;
                     case R.id.challenges:
-                        nFragment = new ChallengeListFragment();
+//                        nFragment = new ChallengeListFragment();
                         break;
                 }
                 replaceFragment(nFragment);
