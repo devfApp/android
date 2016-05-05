c3eb101349a6992683a43fbc80fff8ee604b9e12
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-05 12:43:53 -0500

    item view

--- app/src/main/java/com/icaboalo/devfapp/ui/adapter/OptionRecyclerAdapter.kt
+++ app/src/main/java/com/icaboalo/devfapp/ui/adapter/OptionRecyclerAdapter.kt
@@ -5,8 +5,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.icaboalo.devfapp.R
 import com.icaboalo.devfapp.io.model.OptionApiModel
+import com.squareup.picasso.Picasso
 import java.util.*
 
 /**
@@ -27,14 +30,15 @@ class OptionRecyclerAdapter: RecyclerView.Adapter<OptionRecyclerAdapter.OptionVi
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OptionViewHolder? {
-        val view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        val view = inflater.inflate(R.layout.item_options_adapter, parent, false)
         val viewHolder = OptionViewHolder(view, viewHolderClickListener)
         return viewHolder
     }
 
     override fun onBindViewHolder(holder: OptionViewHolder, position: Int) {
         val option = optionList[position]
-        holder.test!!.text = option.name
+        holder.nameText.text = option.name
+        holder.setImage(option.image!!)
     }
 
     override fun getItemCount(): Int {
@@ -43,16 +47,24 @@ class OptionRecyclerAdapter: RecyclerView.Adapter<OptionRecyclerAdapter.OptionVi
 
     inner class OptionViewHolder: RecyclerView.ViewHolder, View.OnClickListener {
 
-        var test: TextView? = null
+        var nameText: TextView
+        var optionImage: ImageView
         var viewHolderClickListener: OnViewHolderClick
 
         constructor(itemView: View, viewHolderClickListener: OnViewHolderClick) : super(itemView){
-            test = itemView.findViewById(android.R.id.text1) as TextView?
+            nameText = itemView.findViewById(R.id.text_name) as TextView
+            optionImage = itemView.findViewById(R.id.image_option) as ImageView
             this.viewHolderClickListener = viewHolderClickListener
         }
 
         override fun onClick(v: View) {
             viewHolderClickListener.onClick(v, adapterPosition)
+        }
+
+        fun setImage(url: String){
+            if (!url.isEmpty()){
+                Picasso.with(context).load(url).into(optionImage)
+            }
         }
     }
 }
